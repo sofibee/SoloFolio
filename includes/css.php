51449aa21c02c8d9a7628fcd543e388acd96564e
Joel Hawksley <joel@mojotech.com>
2014-02-15 12:48:10 -0500

    Fix bug where over flow was hidden.

--- includes/css.php
+++ includes/css.php
@@ -165,7 +165,7 @@ function solofolio_css() {
 
   $styles .= "
   #wrapper {
-    left: " . (get_theme_mod( 'solofolio_header_width', '200' ) + 40) . "px;
+    left: " . (get_theme_mod( 'solofolio_header_width', '200' ) + 60) . "px;
     width: auto;
   }
 
