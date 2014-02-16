b302af4e81234b9f5aeb6c12a38e927068646d2b
Joel Hawksley <joel@mojotech.com>
2014-02-15 21:52:04 -0500

    Add toggle icons.

--- includes/caption-widget.php
+++ includes/caption-widget.php
@@ -1,28 +1,33 @@
 <?php
 class solofolio_captions extends WP_Widget {
 
-    function solofolio_captions() {
-        parent::WP_Widget(false, $name = 'Solofolio Captions');
-    }
+  function solofolio_captions() {
+    parent::WP_Widget(false, $name = 'Solofolio Captions');
+  }
 
-    function widget($args, $instance) {
-        extract( $args );
-        global $wpdb;
+  function widget($args, $instance) {
+    extract( $args );
+    global $wpdb;
 
-        if(!$size)
-            $size = 40;
+    if(!$size)
+      $size = 40;
 
-        echo $before_widget;
-        echo '<div class="solofolio-cyclereact-sidebar">';
-          	echo '<p class="solofolio-cyclereact-caption"></p>';
-            echo '<span class="solofolio-cyclereact-controls">';
-                echo '<a class="thumbs" href="#"><i class="fa fa-th"></i></a>';
-                echo '<a class="prev" href="#"><i class="fa fa-chevron-left"></i></a>';
-                echo '<a class="next" href="#"><i class="fa fa-chevron-right"></i></a>';
-            echo '</span>';
-        echo '</div>';
-		echo $after_widget;
-    }
+    echo $before_widget;
+    echo '<div class="solofolio-cyclereact-sidebar">';
+      echo '<p class="solofolio-cyclereact-caption"></p>';
+      echo '<span class="solofolio-cyclereact-controls">';
+        echo '<a class="thumbs" href="#">';
+          echo '<i class="fa fa-th"></i>';
+          echo '<i class="fa fa-expand"></i>';
+        echo '</a>';
+        echo '<span class="arrows">';
+          echo '<a class="prev" href="#"><i class="fa fa-chevron-left"></i></a>';
+          echo '<a class="next" href="#"><i class="fa fa-chevron-right"></i></a>';
+        echo '</span>';
+      echo '</span>';
+    echo '</div>';
+	echo $after_widget;
+  }
 }
 add_action('widgets_init', create_function('', 'return register_widget("solofolio_captions");'));
 ?>
--- includes/gallery/js/cyclereact.js
+++ includes/gallery/js/cyclereact.js
@@ -22,23 +22,19 @@ jQuery(window).load(function(){
   setResponsive();
   $(".thumbs").click(function(){
     $("#solofolio-cyclereact-thumbs").toggle();
-    $(".solofolio-cyclereact-controls p").toggle();
+    $(".solofolio-cyclereact-caption").toggle();
     $("#solofolio-cyclereact-stage").toggle();
-    $(".toggle-thumbs").toggleClass("show-full");
-    $(".toggle-arrows").toggleClass("show-arrows");
+    $(".thumbs").toggleClass("show-full");
+    $(".arrows").toggle();
   });
   $(".thumb a").click(function(){
     $("#solofolio-cyclereact-thumbs").hide();
-    $("#solofolio-cyclereact-stage").show();
+    $("#solofolio-cyclereact-stage, .arrows").show();
+    $(".thumbs").removeClass("show-full");
   });
 });
 
 jQuery(window).resize(setResponsive);
-
-// jQuery( '#solofolio-cyclereact-images' ).on( 'cycle-after', function( event, opts ) {
-//   $("#solofolio-cyclereact-thumbs").hide();
-//   $("#solofolio-cyclereact-stage").show();
-// });
 
 jQuery( '#solofolio-cyclereact-images' ).on( 'cycle-before', function( event, opts ) {
   window.picturefill();
