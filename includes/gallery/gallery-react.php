958509f40889226353cfe5dcbca1cdf1c7bec39c
Joel Hawksley <joel@mojotech.com>
2014-03-25 12:56:12 -0400

    Fix React gallery bug.

--- includes/gallery/gallery-react.php
+++ includes/gallery/gallery-react.php
@@ -16,7 +16,7 @@ foreach ($attachment_ids as $id) {
 		<div data-src=\"" . $link5[0] . "\" data-media=\"(min-width: " . $helper . "px)\"></div>
 
 		<!-- Fallback content for non-JS browsers. Same img src as the initial, unqualified source element. -->
-		<noscript><img src=\"" . $link6[0] . "\" alt=\"" .  wptexturize($attachment->post_excerpt) . "\"></noscript>
+		<noscript><img src=\"" . $link4[0] . "\" alt=\"" .  wptexturize($attachment->post_excerpt) . "\"></noscript>
 	</div>
 	<p class=\"solofolio-caption sl-react-caption\">" .  wptexturize($attachment->post_excerpt) . "</p>";
 } // End ForEach
@@ -24,16 +24,16 @@ foreach ($attachment_ids as $id) {
 add_action('wp_footer', 'sl_react');
 
 function sl_react() {
-	$output .= "<script type=\"text/javascript\">window.matchMedia=window.matchMedia||(function(e,f){var c,a=e.documentElement,b=a.firstElementChild||a.firstChild,d=e.createElement(\"body\"),g=e.createElement(\"div\");g.id=\"mq-test-1\";g.style.cssText=\"position:absolute;top:-100em\";d.appendChild(g);return function(h){g.innerHTML='&shy;<style media=\"'+h+'\"> #mq-test-1 { width: 42px; }</style>';a.insertBefore(d,b);c=g.offsetWidth==42;a.removeChild(d);return{matches:c,media:h}}})(document);</script>";
+	$output = "<script type=\"text/javascript\">window.matchMedia=window.matchMedia||(function(e,f){var c,a=e.documentElement,b=a.firstElementChild||a.firstChild,d=e.createElement(\"body\"),g=e.createElement(\"div\");g.id=\"mq-test-1\";g.style.cssText=\"position:absolute;top:-100em\";d.appendChild(g);return function(h){g.innerHTML='&shy;<style media=\"'+h+'\"> #mq-test-1 { width: 42px; }</style>';a.insertBefore(d,b);c=g.offsetWidth==42;a.removeChild(d);return{matches:c,media:h}}})(document);</script>";
 	$output .= "<script type=\"text/javascript\" src=\"" . get_template_directory_uri() . "/includes/gallery/js/picturefill.js\"></script>";
 
 	$output .= "
 	<script type=\"text/javascript\">
 	$(window).load(function(){
 		var setResponsive = function () {
-		  var pageHeight = jQuery(window).height();
+		  var pageHeight = $(window).height();
 		  var blockHeight = $(\".sl-react-caption\").outerHeight();
-		  $('.sl-react img').css('max-height', pageHeight - blockHeight);
+		  $('.sl-react img').css('max-height', pageHeight - blockHeight - 40);
 		}
 		$(window).resize(setResponsive);
 		setResponsive();
