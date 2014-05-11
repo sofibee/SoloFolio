636abf1337d928e93b86ed1687bd2b548cae8489
Joel Hawksley <joel@mojotech.com>
2014-05-11 15:02:22 -0400

    Refine layout of About page template.

--- about.php
+++ about.php
@@ -4,12 +4,14 @@
 
 <div id="content-about">
 	<?php if (have_posts()) : ?>
-	<?php the_post_thumbnail('medium');; ?>
-		<?php while (have_posts()) : the_post(); ?>
-			<div id="content-about-right">
-				<?php the_content('Read the rest of this entry &raquo;'); ?>
-			</div>
-		<?php endwhile; ?>
+    <div id="content-about-image">
+      <?php the_post_thumbnail('medium');; ?>
+  	</div>
+  	<?php while (have_posts()) : the_post(); ?>
+  		<div id="content-about-content">
+  			<?php the_content('Read the rest of this entry &raquo;'); ?>
+  		</div>
+  	<?php endwhile; ?>
 	<?php endif; ?>
 </div>
 
