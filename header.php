d331253a6244394d792add231314b6ff5b47defa
Joel Hawksley <joel@mojotech.com>
2014-05-23 19:57:35 -0400

    [REGRESSION] Fix favicon.

--- header.php
+++ header.php
@@ -16,6 +16,7 @@ SoloFolio v6.20140330 - github.com/joelhawksley/SoloFolio
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, minimal-ui" />
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+	<link rel='shortcut icon' type='image/x-icon' href='<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico' />
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
